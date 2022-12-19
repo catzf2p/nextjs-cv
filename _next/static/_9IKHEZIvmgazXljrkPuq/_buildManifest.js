25e9b6e50f48b8e56236e8af3e39f979ad354895
catzf2p <catzf2p@users.noreply.github.com>
2022-12-19 06:50:23 +0000

    deploy: 6ccd8dec794e8cf46814d18c8b80069d27acd9dc

---
+++ _next/static/_9IKHEZIvmgazXljrkPuq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/966-885852da238a5bee.js","static/css/d0320de32467736e.css","static/chunks/pages/index-bfb08e9c7b33311e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
