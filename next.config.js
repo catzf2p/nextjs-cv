02448cf5591c87df10288497d940e0323f94a4fb
catzf2p <catzf2p@gmail.com>
2022-12-19 13:49:44 +0700

    update cfg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    assetPrefix: isProd ? '/nextjs-tailwind-starter/' : '',
+    assetPrefix: isProd ? '/nextjs-tailwind-resume/' : '',
     jsconfigPaths: true,
     reactStrictMode: true,
     images: {
