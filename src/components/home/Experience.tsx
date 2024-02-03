81b4137efd715d133581bde12d1d813e97fba83d
catzf2p <catzf2p@gmail.com>
2024-02-03 20:42:43 +0700

    fix: box size not away full when animation

--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -15,101 +15,105 @@ export const Experience: React.FC = () => {
 
   return (
     <>
-      <ScrollAnimation
-        animateOnce={true}
-        animateIn='animate__fadeInUp'
-        animateOut='animate__fadeOutUp'
-      >
-        <h2 className='inline-flex items-center text-3xl font-normal'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='mr-4'
-            width='48'
-            height='48'
-            viewBox='0 0 24 24'
-            strokeWidth='1'
-            stroke='currentColor'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <path
-              stroke='none'
-              d='M0 0h24v24H0z'
+      <div className='w-ful min-w-full'>
+        <ScrollAnimation
+          animateOnce={true}
+          animateIn='animate__fadeInUp'
+          animateOut='animate__fadeOutUp'
+        >
+          <h2 className='inline-flex items-center text-3xl font-normal'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='mr-4'
+              width='48'
+              height='48'
+              viewBox='0 0 24 24'
+              strokeWidth='1'
+              stroke='currentColor'
               fill='none'
-            ></path>
-            <line
-              x1='3'
-              y1='19'
-              x2='21'
-              y2='19'
-            ></line>
-            <rect
-              x='5'
-              y='6'
-              width='14'
-              height='10'
-              rx='1'
-            ></rect>
-          </svg>
-          <span className='bg-gradient-to-r from-purple-500 to-blue-700 bg-clip-text text-3xl font-bold text-transparent'>Experience</span>
-        </h2>
-      </ScrollAnimation>
-      <div className=''>
-        <ol className='relative ml-6 mt-4 border-l border-slate-800/80'>
-          {jobs?.map((job: IJob) => {
-            return (
-              <>
-                <ScrollAnimation
-                  animateOnce={true}
-                  animateIn='animate__fadeInUp'
-                  animateOut='animate__fadeOutUp'
-                >
-                  <li className='group relative mb-10 pl-6'>
-                    <div className='absolute top-1 -left-2 h-4 w-4 rounded-full bg-blue-600 group-hover:animate-ping'></div>
-                    <div className='absolute top-2 -left-1 h-2 w-2 rounded-full border-transparent bg-white ring-2 ring-blue-600/40 ring-offset-4 ring-offset-blue-600'></div>
-                    <time className='mb-1 text-sm font-normal leading-none text-slate-600'>
-                      {job.startAt} - {job.endAt} | {job.company}
-                    </time>
-                    <h3 className='my-1 text-lg font-bold'>{job.position}</h3>
-                    <ul>
-                      {job.responsibilities.map((responsibility: string) => {
-                        return (
-                          <li key={shortid.generate()}>
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              className='-ml-2 inline text-blue-600'
-                              width='24'
-                              height='24'
-                              viewBox='0 0 24 24'
-                              strokeWidth='1'
-                              stroke='currentColor'
-                              fill='none'
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                            >
-                              <path
-                                stroke='none'
-                                d='M0 0h24v24H0z'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path
+                stroke='none'
+                d='M0 0h24v24H0z'
+                fill='none'
+              ></path>
+              <line
+                x1='3'
+                y1='19'
+                x2='21'
+                y2='19'
+              ></line>
+              <rect
+                x='5'
+                y='6'
+                width='14'
+                height='10'
+                rx='1'
+              ></rect>
+            </svg>
+            <span className='bg-gradient-to-r from-purple-500 to-blue-700 bg-clip-text text-3xl font-bold text-transparent'>
+              Experience
+            </span>
+          </h2>
+        </ScrollAnimation>
+        <div className=''>
+          <ol className='relative ml-6 mt-4 border-l border-slate-800/80'>
+            {jobs?.map((job: IJob) => {
+              return (
+                <>
+                  <ScrollAnimation
+                    animateOnce={true}
+                    animateIn='animate__fadeInUp'
+                    animateOut='animate__fadeOutUp'
+                  >
+                    <li className='group relative mb-10 pl-6'>
+                      <div className='absolute top-1 -left-2 h-4 w-4 rounded-full bg-blue-600 group-hover:animate-ping'></div>
+                      <div className='absolute top-2 -left-1 h-2 w-2 rounded-full border-transparent bg-white ring-2 ring-blue-600/40 ring-offset-4 ring-offset-blue-600'></div>
+                      <time className='mb-1 text-sm font-normal leading-none text-slate-600'>
+                        {job.startAt} - {job.endAt} | {job.company}
+                      </time>
+                      <h3 className='my-1 text-lg font-bold'>{job.position}</h3>
+                      <ul>
+                        {job.responsibilities.map((responsibility: string) => {
+                          return (
+                            <li key={shortid.generate()}>
+                              <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                className='-ml-2 inline text-blue-600'
+                                width='24'
+                                height='24'
+                                viewBox='0 0 24 24'
+                                strokeWidth='1'
+                                stroke='currentColor'
                                 fill='none'
-                              ></path>
-                              <circle
-                                cx='12'
-                                cy='12'
-                                r='4'
-                              ></circle>
-                            </svg>
-                            <span className='align-middle'>{responsibility}</span>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </li>
-                </ScrollAnimation>
-              </>
-            );
-          })}
-        </ol>
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                              >
+                                <path
+                                  stroke='none'
+                                  d='M0 0h24v24H0z'
+                                  fill='none'
+                                ></path>
+                                <circle
+                                  cx='12'
+                                  cy='12'
+                                  r='4'
+                                ></circle>
+                              </svg>
+                              <span className='align-middle'>{responsibility}</span>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </li>
+                  </ScrollAnimation>
+                </>
+              );
+            })}
+          </ol>
+        </div>
       </div>
     </>
   );
--- src/components/home/KnowMeMore.tsx
+++ src/components/home/KnowMeMore.tsx
@@ -5,77 +5,79 @@ import { AgeCalculator } from '@/utils';
 export const KnowMeMore: React.FC = () => {
   return (
     <>
-      <ScrollAnimation
-        animateOnce={true}
-        animateIn='animate__fadeInUp'
-        animateOut='animate__fadeOutUp'
-        offset={0}
-      >
-        <h2 className='inline-flex items-center text-3xl font-normal'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='mr-4'
-            width='48'
-            height='48'
-            viewBox='0 0 24 24'
-            strokeWidth='1'
-            stroke='currentColor'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <path
-              stroke='none'
-              d='M0 0h24v24H0z'
-              fill='none'
-            ></path>
-            <path d='M16.95 9.01c3.02 .739 5.05 2.123 5.05 3.714c0 2.367 -4.48 4.276 -10 4.276s-10 -1.909 -10 -4.276c0 -1.59 2.04 -2.985 5.07 -3.724'></path>
-            <path d='M7 9c0 1.105 2.239 2 5 2s5 -.895 5 -2v-.035c0 -2.742 -2.239 -4.965 -5 -4.965s-5 2.223 -5 4.965v.035'></path>
-            <path d='M15 17l2 3'></path>
-            <path d='M8.5 17l-1.5 3'></path>
-            <path d='M12 14h.01'></path>
-            <path d='M7 13h.01'></path>
-            <path d='M17 13h.01'></path>
-          </svg>
-          <span className='bg-gradient-to-r from-purple-500 to-blue-700 bg-clip-text text-3xl font-bold text-transparent'>
-            Know Me More
-          </span>
-        </h2>
-      </ScrollAnimation>
-      <div className=''>
+      <div className='w-ful min-w-full'>
         <ScrollAnimation
           animateOnce={true}
           animateIn='animate__fadeInUp'
           animateOut='animate__fadeOutUp'
+          offset={0}
         >
-          <div className='mt-6 grid grid-cols-3 gap-6'>
-            <div className='col-span-3 xl:col-span-2'>
-              <p>
-                &emsp;Hello! I’m Nat Mancharoen. Senior Full Stack Developer specializing in front end development. Experienced with all
-                stages of the development cycle for dynamic web projects. Well-versed in numerous programming languages including
-                JavaScript, SQL, and C#.NET. Strong background in project management and customer relations.
-              </p>
-            </div>
-            <div className='col-span-3 grid grid-cols-1 gap-4 xl:col-span-1 xl:gap-0'>
-              <div className='flex leading-none'>
-                <span className='w-24 font-medium'>Age:</span>
-                <span>{AgeCalculator(new Date(1984, 6, 22))} (June, 1984)</span>
-              </div>
-              <div className='flex leading-3'>
-                <span className='w-24 font-medium leading-3'>Address:</span>
-                <span className='leading-3'>Tha Chang Sing Buri 16140</span>
-              </div>
-              <div className='flex leading-3'>
-                <span className='w-24 font-medium leading-3'>E-Mail:</span>
-                <span className='leading-3'>monzgamez@gmail.com</span>
-              </div>
-              <div className='flex leading-none'>
-                <span className='w-24 font-medium leading-3'>Phone:</span>
-                <span className='leading-3'>088-020-2322</span>
-              </div>
-            </div>
-          </div>
+          <h2 className='inline-flex items-center text-3xl font-normal'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='mr-4'
+              width='48'
+              height='48'
+              viewBox='0 0 24 24'
+              strokeWidth='1'
+              stroke='currentColor'
+              fill='none'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path
+                stroke='none'
+                d='M0 0h24v24H0z'
+                fill='none'
+              ></path>
+              <path d='M16.95 9.01c3.02 .739 5.05 2.123 5.05 3.714c0 2.367 -4.48 4.276 -10 4.276s-10 -1.909 -10 -4.276c0 -1.59 2.04 -2.985 5.07 -3.724'></path>
+              <path d='M7 9c0 1.105 2.239 2 5 2s5 -.895 5 -2v-.035c0 -2.742 -2.239 -4.965 -5 -4.965s-5 2.223 -5 4.965v.035'></path>
+              <path d='M15 17l2 3'></path>
+              <path d='M8.5 17l-1.5 3'></path>
+              <path d='M12 14h.01'></path>
+              <path d='M7 13h.01'></path>
+              <path d='M17 13h.01'></path>
+            </svg>
+            <span className='bg-gradient-to-r from-purple-500 to-blue-700 bg-clip-text text-3xl font-bold text-transparent'>
+              Know Me More
+            </span>
+          </h2>
         </ScrollAnimation>
+        <div className=''>
+          <ScrollAnimation
+            animateOnce={true}
+            animateIn='animate__fadeInUp'
+            animateOut='animate__fadeOutUp'
+          >
+            <div className='mt-6 grid grid-cols-3 gap-6'>
+              <div className='col-span-3 xl:col-span-2'>
+                <p>
+                  &emsp;Hello! I’m Nat Mancharoen. Senior Full Stack Developer specializing in front end development. Experienced with all
+                  stages of the development cycle for dynamic web projects. Well-versed in numerous programming languages including
+                  JavaScript, SQL, and C#.NET. Strong background in project management and customer relations.
+                </p>
+              </div>
+              <div className='col-span-3 grid grid-cols-1 gap-4 xl:col-span-1 xl:gap-0'>
+                <div className='flex leading-none'>
+                  <span className='w-24 font-medium'>Age:</span>
+                  <span>{AgeCalculator(new Date(1984, 6, 22))} (June, 1984)</span>
+                </div>
+                <div className='flex leading-3'>
+                  <span className='w-24 font-medium leading-3'>Address:</span>
+                  <span className='leading-3'>Tha Chang Sing Buri 16140</span>
+                </div>
+                <div className='flex leading-3'>
+                  <span className='w-24 font-medium leading-3'>E-Mail:</span>
+                  <span className='leading-3'>monzgamez@gmail.com</span>
+                </div>
+                <div className='flex leading-none'>
+                  <span className='w-24 font-medium leading-3'>Phone:</span>
+                  <span className='leading-3'>088-020-2322</span>
+                </div>
+              </div>
+            </div>
+          </ScrollAnimation>
+        </div>
       </div>
     </>
   );
